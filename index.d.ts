1993f36484e6ad24dbcbec8f4be8939a8b0c65f1
Danil <komarov.fl@yandex.ru>
2018-12-07 12:26:03 +0300

    init

---
+++ index.d.ts
@@ -0,0 +1,47 @@
+interface ConnectionConfig {
+  hostname: string,
+  port: string | number,
+  username: string,
+  password: string
+}
+
+interface Config {
+  exchangeName: string,
+  rpcQueueName: string,
+  connectionConfig: ConnectionConfig,
+  ttl: number
+}
+
+interface Request {
+  method: string;
+  params?: any[];
+}
+
+interface Response {
+  id: string;
+  result?: any;
+  error?: any;
+}
+
+type Event =
+  | 'orderCreate'
+  | 'orderClose'
+  | 'orderCancel'
+  | 'orderError'
+  | 'taskCreate'
+  | 'taskClose'
+  | 'taskError';
+
+interface EventData {
+  event: Event;
+  data?: any;
+}
+
+declare class DweClient {
+  constructor(config: Config);
+  connect(): Promise<void>;
+  call(req: Request): Promise<Response>;
+  on(event: Event, fn: (data: any) => void): void;
+}
+
+export = DweClient;
---
+++ src/DweClient.js
@@ -0,0 +1,98 @@
+const amqplib = require('amqplib');
+const EventEmitter = require('events');
+const uuidv4 = require('uuid/v4');
+
+class DweClient extends EventEmitter {
+  constructor(config) {
+    super();
+    this.exchangeName = config.exchangeName;
+    this.rpcQueueName = config.rpcQueueName;
+    this.connectionConfig = config.connectionConfig;
+    this.ttl = config.ttl;
+
+    this.responseListeners = [];
+  }
+
+  async connect() {
+    this.connection = await amqplib.connect(this.connectionConfig);
+    this.channel = await this.connection.createChannel();
+
+    let assertResult = await this.channel.assertQueue('', { exclusive: true });
+    this.replyQueueName = assertResult.queue;
+
+    await this.channel.assertExchange(this.exchangeName, 'fanout', { durable: false });
+    assertResult = await this.channel.assertQueue('', { exclusive: true });
+    this.eventsQueueName = assertResult.queue;
+    this.channel.bindQueue(this.eventsQueueName, this.exchangeName, '');
+
+    this.channel.consume(this.eventsQueueName, msg => this.onMessage(msg), { noAck: true });
+    this.channel.consume(this.replyQueueName, msg => this.onResponse(msg), { noAck: true });
+  }
+
+  addResponseListener(id, resolve, reject) {
+    this.responseListeners = [
+      ...this.responseListeners,
+      {
+        id,
+        resolve,
+        reject,
+        timeout: setTimeout(() => {
+          this.responseListeners = this.responseListeners.filter(r => r.id !== id);
+          reject(new Error('Response timeout'));
+        }, this.ttl),
+      },
+    ];
+  }
+
+  async call(req) {
+    const corrId = uuidv4();
+    const stringifyed = JSON.stringify(req);
+
+    return new Promise(async (resolve, reject) => {
+      await this.channel.sendToQueue(this.rpcQueueName, Buffer.from(stringifyed), {
+        correlationId: corrId, replyTo: this.replyQueueName, expiration: this.ttl,
+      });
+      this.addResponseListener(corrId, resolve, reject);
+    });
+  }
+
+  onResponse(msg) {
+    const id = msg.properties.correlationId;
+    const response = this.responseListeners.find(r => r.id === id);
+
+    if (!response) {
+      return;
+    }
+
+    const data = JSON.parse(msg.content.toString());
+    this.responseListeners = this.responseListeners.filter(r => r.id !== id);
+    clearTimeout(response.timeout);
+    response.resolve(data);
+  }
+
+  onMessage(msg) {
+    try {
+      const data = JSON.parse(msg.content.toString());
+      this.emit(data.event, data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  eventSend(event, data) {
+    const eventData = {
+      event,
+      data,
+    };
+    const stringifyed = JSON.stringify(eventData);
+    this.channel.publish(this.eventsExchange, '', Buffer.from(stringifyed), { expiration: this.ttl });
+  }
+
+  async close() {
+    if (this.connection) {
+      await this.connection.close();
+    }
+  }
+}
+
+module.exports = DweClient;
